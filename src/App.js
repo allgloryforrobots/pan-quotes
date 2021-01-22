39be792dcca040151380d47288f9173f555cad20
allgloryforrobots <75021483+allgloryforrobots@users.noreply.github.com>
2021-01-22 09:01:30 +0300

    'master'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import 'antd/dist/antd.css'
+import React, {useState} from "react"
+import {Button, Layout, Space, Spin} from 'antd'
+import { HeartTwoTone } from '@ant-design/icons'
+import {Card, message} from 'antd'
+import ModalAsync from "./ModalAsunc"
+import firebase from "firebase/app"
+import 'firebase/firestore'
+import {useCollection} from "react-firebase-hooks/firestore"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const {Content, Footer} = Layout
+
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId,
 }
 
-export default App;
+//Initialize Firebase
+if (!firebase.apps.length) {
+    try {
+        firebase.initializeApp(firebaseConfig)
+    } catch (err) {
+        console.error('Firebase initialization error raised', err.stack)
+    }
+}
+
+function App() {
+
+    const messagesRef = firebase.firestore().collection('quotes')
+    const query = messagesRef.limit(25)
+    const [quotes, loading, error] = useCollection(query)
+    const [izbr, setIzbr] = useState('normal')
+
+    const onHandleClick = () => {
+
+    }
+
+    return (
+        <Layout className="layout">
+            <div style={{
+                background: '#001529',
+                height: '60px',
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center'
+            }}>
+                <div style={{marginLeft: 40}}>
+                    <h2 style={{color: 'white', whiteSpace: 'nowrap', margin: 0}}>iЦитата 🔥</h2>
+                </div>
+
+                <div>
+                    {
+                        izbr === 'normal' && <Button type="link"
+                                                     onClick={() => setIzbr('izbr')}
+                                                     style={{color: 'rgb(255,165,0)'}}>
+                            Полюбившиеся</Button>
+                    }
+                    {
+                        izbr === 'izbr' && <Button type="link"
+                                                   onClick={() => setIzbr('normal')}
+                                                   style={{color: 'rgb(255,165,0)'}}>
+                            Все цитаты</Button>
+                    }
+                    <ModalAsync/>
+                </div>
+            </div>
+
+                <Content style={{padding: '15px 25px', minHeight: '100vh'}}>
+                    <Space size={[4, 16]} wrap>
+                        {
+                            quotes ? quotes.docs.filter(quote => {
+                                if (izbr === 'izbr') {
+                                    return Object.keys(localStorage).indexOf(quote.id) > -1
+                                }
+                                return true
+
+                            }).map(quote => {
+                            return <Card title={quote.data().author}
+                                         key={Math.random()}
+                                  bordered={false}
+                                  extra={
+                                      <span>
+                                        <HeartTwoTone
+                                            onClick={() => localStorage.setItem(quote.id, quote.id)}
+                                            style={{ fontSize: '16px', marginRight: 15}}/>
+                                      </span>
+                                  }
+                                  style={{width: 300, margin: 10}}>
+                                <p>{quote.data().quote}</p>
+                            </Card>
+                        }) : <Spin/>
+                        }
+
+                        {
+                            error && message.info(JSON.stringify(error))
+                        }
+
+                    </Space>
+                </Content>
+
+
+            <Footer style={{ textAlign: 'center' }}> 2021 © Created by IT-дизель</Footer>
+        </Layout>
+)
+}
+
+export default App
---
+++ src/ModalAsunc.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import {Button, Form, Input, Modal} from 'antd'
+import firebase from "firebase/app"
+import 'firebase/firestore'
+
+
+
+const ModalAsync = () => {
+    const layout = {
+        labelCol: { span: 5 },
+        wrapperCol: { span: 16 },
+    }
+    const onValuesChange = values => {
+        if (values.author) {
+            setAuthor(values.author)
+        }
+        if (values.quote) {
+            setQuote(values.quote)
+        }
+    }
+
+    const [visible, setVisible] = React.useState(false)
+    const [confirmLoading, setConfirmLoading] = React.useState(false)
+    const [author, setAuthor] = React.useState('')
+    const [quote, setQuote] = React.useState('')
+
+
+    const showModal = () => {
+        setVisible(true)
+    }
+
+    const handleOk = () => {
+
+        firebase.firestore().collection('quotes').add({
+            author,
+            quote,
+        })
+        setAuthor('')
+        setQuote('')
+        setConfirmLoading(true)
+        setTimeout(() => {
+            setVisible(false)
+            setConfirmLoading(false)
+        }, 0)
+    }
+
+    const handleCancel = () => {
+        setVisible(false)
+    }
+
+    return (
+        <>  <Button type="link"
+                    onClick={showModal}
+                    style={{color: 'rgb(255,165,0)'}}>Сбацай цитату</Button>
+            <Modal
+                title="Закинуть цитату в печь"
+                visible={visible}
+                onOk={handleOk}
+                confirmLoading={confirmLoading}
+                onCancel={handleCancel}
+                destroyOnClose
+            >
+                <Form {...layout} name="nest-messages" onValuesChange={onValuesChange} >
+
+                    <Form.Item name={'author'} label="Автор">
+                        <Input/>
+                    </Form.Item>
+                    <Form.Item name={'quote'} label="Изречение">
+                        <Input.TextArea/>
+                    </Form.Item>
+                </Form>
+            </Modal>
+        </>
+    )
+}
+
+export default ModalAsync
